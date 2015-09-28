696b72adc66e0079f1cee3ab5c7efcfe4875e0fc
Eirik Årdal <eaardal@outlook.com>
2015-09-28 21:03:57 +0200

    Initial commit and completed app

---
+++ MyIP/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace MyIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var location = GetGeoLocation();
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+                var json = JsonConvert.SerializeObject(location, Formatting.Indented);
+                Console.WriteLine(json);
+
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex);
+                Console.ResetColor();
+            }
+        }
+
+        private static GeoLocation GetGeoLocation()
+        {
+            var client = new RestClient(new Uri("http://freegeoip.net/json/"));
+            var request = new RestRequest(Method.GET);
+            var response = client.Execute<GeoLocation>(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return response.Data;
+            }
+            return null;
+        }
+    }
+
+    internal class GeoLocation
+    {
+        public string Ip { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string RegionCode { get; set; }
+        public string RegionName { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+        public string MetroCode { get; set; }
+        public string AreaCode { get; set; }
+    }
+}
